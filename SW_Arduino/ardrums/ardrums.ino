d46c22358beb42d4d79b8df81cabe77b54559b46
Pablo Clemente <paclema@gmail.com>
2016-02-16 16:46:39 +0100

    Updated max-mins on an array

--- SW_Arduino/ardrums/ardrums.ino
+++ SW_Arduino/ardrums/ardrums.ino
@@ -11,11 +11,11 @@
  
 #define PIN 6
 #define LEDPIN     13     // status LED pin
-#define PIEZOTHRESHOLD 5  // analog threshold for piezo sensing
+#define PIEZOTHRESHOLD 70  // analog threshold for piezo sensing
 #define PADNUM 1          // number of pads
 
 int pad_val[PADNUM];
-int val_max = 5, val_min = 1023;
+int val_max[PADNUM], val_min[PADNUM];
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(33, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -24,8 +24,29 @@ void setup() {
   pinMode(LEDPIN, OUTPUT);
   Serial.begin(19200);   // set serial output rate
   
+  for(int i = 0; i < PADNUM; i++){
+  	val_max[i] =0;
+  	val_min[i] =1023;
+  }
+
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+
+  pinta(255,0,0);
+  delay(200);
+
+  pinta(255,255,0);
+  delay(400);
+  pinta(0,255,0);
+  delay(400);
+  pinta(0,255,255);
+  delay(400);
+  pinta(0,0,255);
+  delay(400);
+
+  pinta(255,255,255);
+  delay(1000);
+  pinta(0,0,0);     
 }
 
 void loop() {
@@ -36,32 +57,38 @@ void loop() {
   for(int i = 0; i < PADNUM; i++) pad_val[i] = analogRead(i);
 
   for(int i = 0; i < PADNUM; i++){
-/*
-    if(val[i]>=val_max) val_max = val;
-    if(val<=val_min) val_min = val;
 
-      Serial.print("PAD: ");
-      Serial.print(val);
-      Serial.print("\tMax :");
-      Serial.print(val_max);
-      Serial.print("\tMin :");
-      Serial.println(val_min);
-*/
+    if(pad_val[i]>=val_max[i]) val_max[i] = pad_val[i];
+    if(pad_val[i]<=val_min[i]) val_min[i] = pad_val[i];
+
     if( pad_val[i] >= PIEZOTHRESHOLD ) {
 
+    	Serial.print("PAD: ");
+	    Serial.print(pad_val[i]);
+	    Serial.print("\tMax :");
+	    Serial.print(val_max[i]);
+	    Serial.print("\tMin :");
+	    Serial.println(val_min[i]);
+
     	digitalWrite(LEDPIN,HIGH);  // indicate we're sending MIDI data
-    	digitalWrite(LEDPIN,LOW);
+    	int val=map(pad_val[i],val_min[i],600,0,255);
 
-    	int val=map(pad_val[i],0,1023,0,255);
-
-    	for(int i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, strip.Color(val, 0, 0));
+    	for(int i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, strip.Color(val, val, 0));
     	strip.show();
-  		delay(50);
+  		delay(val);
 
     	for(int i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, strip.Color(0, 0, 0));
-    	strip.show();      
+    	strip.show();
+
+    	digitalWrite(LEDPIN,LOW);      
     }
   }
 
 
+}
+
+
+void pinta(int r, int g, int b){
+	for(int i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, strip.Color(r, g, b));
+	strip.show();
 }
